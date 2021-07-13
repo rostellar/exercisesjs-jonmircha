3b5289338620796e02f26c8442cbf686386e2ab9
Rostellar <emivirdisla@gmail.com>
2021-07-12 20:55:25 -0400

    Resueltos los ejercicios 1-5

--- script.js
+++ script.js
@@ -1,9 +1,70 @@
 // 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+
+const contarCaracteres = (cadena1) => {
+    if (typeof cadena1 === "string") {
+        console.log(cadena1.length)
+    } else {
+        console.warn('Debes ingresar una cadena de texto');
+    }
+}
+
+//contarCaracteres('Helllo world');
+
 // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+
+const textoRecortado = (cadena2, trozo = 0) => {
+    if (typeof cadena2 === "string" && typeof trozo === "number") {
+        console.log(cadena2.slice(0, trozo))
+    } else {
+        console.warn('Datos erróneos')
+    }
+}
+
+//textoRecortado('Hello world', 5);
+
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+
+const textoSeparado = (cadena3) => {
+    if (typeof cadena3 === "string") {
+        console.log(cadena3.split(" "))
+    } else {
+        console.warn('Debes ingresar una cadena de texto.')
+    }
+}
+
+//textoSeparado("Hello world, this is a test")
+
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+
+const repetirTexto = (cadena4, repeticiones) => {
+    if (typeof cadena4 === "string" && typeof repeticiones === "number") {
+        for (let i = 0; i < repeticiones; i++) {
+            console.log(cadena4)
+        }
+    } else {
+        console.warn('Datos erróneos')
+    }
+}
+
+//repetirTexto('Hello world', 2)
+
 // 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+
+const invertirCadena = (cadena5) => {
+    if (typeof cadena5 === "string") {
+        cadenaInvertida = cadena5.split("").reverse().join('')
+        console.log(cadenaInvertida);
+    } else {
+        console.warn('Debes ingresar una cadena de texto.')
+    }
+}
+
+//invertirCadena("Hello world")
+
 // 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+
+
+
 // 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
