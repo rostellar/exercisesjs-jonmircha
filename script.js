6e4a08d668af430c62d1e5e856203705caafef17
Emivir Rojas <emivirdisla@gmail.com>
2021-08-02 23:24:24 -0400

    Resueltos ejercicios 10 y 11

--- script.js
+++ script.js
@@ -109,10 +109,40 @@ const eliminaPatrones = (texto, eliminar) => {
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 
 const randNum = () => {console.log(Math.random()+501)}
-randNum()
+//randNum()
 
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+
+const capicua = (numCapicua) => {
+    if (typeof numCapicua === "number") {
+        let numCapicuaStr = numCapicua.toString();
+        let numCapicuaStrSplit = numCapicuaStr.split("");
+        let numCapicuaReverse = numCapicuaStrSplit.reverse();
+        let numCapicuaJoin = numCapicuaReverse.join("");
+        
+        numCapicuaJoin === numCapicuaStr ? console.log('El número es capicua') : console.log("El número no es capicua");
+    } else {
+        console.warn('Debes ingresar un número.');
+    }
+}
+
+//capicua(404);
+
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+
+const factorial = (numerofactorial) => {
+    if (typeof numerofactorial === "number") {
+        for (let counter = numerofactorial - 1; counter > 0; counter--) {
+            numerofactorial = numerofactorial * counter
+        }
+        console.log(numerofactorial);
+    } else {
+        console.warn('Por favor, ingresa un número');
+    }
+}
+
+factorial(6)
+
 // 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
 // 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
